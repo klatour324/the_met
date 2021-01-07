ec8a92273023919716cc32493db988eb1a4a761c
Katy La Tour <klatour324@gmail.com>
2021-01-07 10:20:41 -0600

    Create test for interests

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -12,4 +12,20 @@ class PatronTest < Minitest::Test
     assert_equal "Bob", patron_1.name
     assert_equal 20, patron_1.spending_money
   end
+
+  def test_it_has_no_interests_to_start
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal [], patron_1.interests
+  end
 end
+
+# pry(main)> patron_1.interests
+# # => []
+#
+# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
+#
+# pry(main)> patron_1.add_interest("Gems and Minerals")
+#
+# pry(main)> patron_1.interests
+# # => ["Dead Sea Scrolls", "Gems and Minerals"]
