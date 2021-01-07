f986fb367d58bcbc77c45ddbc38ce2b1863220e4
Katy La Tour <klatour324@gmail.com>
2021-01-07 11:55:03 -0600

    Create test to group patrons by exhibit

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -77,21 +77,43 @@ class MuseumTest < Minitest::Test
 
     assert_equal [patron_1, patron_2, patron_3], dmns.patrons
   end
+
+  def test_it_can_group_patrons_by_exhibit_interest
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    patron_1 = Patron.new("Bob", 20)
+    patron_2 = Patron.new("Sally", 20)
+    patron_3 = Patron.new("Johnny", 5)
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2.add_interest("IMAX")
+    patron_3.add_interest("Dead Sea Scrolls")
+
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    expected = {
+                  gems_and_minerals => [patron_1],
+                  dead_sea_scrolls => [patron_1, patron_3],
+                  imax => [patron_2]
+                }
+
+    assert_equal expected, dmns.patrons_by_exhibit_interest
+  end
 end
 
 
 
-# #Patrons are added even if they don't have enough money for all/any exhibits.
-#
-# pry(main)> dmns.patrons_by_exhibit_interest
-# # =>
-# # {
-# #   #<Exhibit:0x00007fb202238618...> => [#<Patron:0x00007fb2011455b8...>],
-# #   #<Exhibit:0x00007fb202248748...> => [#<Patron:0x00007fb2011455b8...>, #<Patron:0x00007fb20227f8b0...>, #<Patron:0x6666fb20114megan...>],
-# #   #<Exhibit:0x00007fb20225f8d0...> => []
-# # }
-#
-# pry(main)> dmns.ticket_lottery_contestants(dead_sea_scrolls)
+
+#(main)> dmns.ticket_lottery_contestants(dead_sea_scrolls)
 # # => [#<Patron:0x00007fb2011455b8...>, #<Patron:0x6666fb20114megan...>]
 #
 # pry(main)> dmns.draw_lottery_winner(dead_sea_scrolls)
